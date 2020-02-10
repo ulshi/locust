1941d1bf846cb3dd878c257e11636f594927fa6b
ulshi <46480421+ulshi@users.noreply.github.com>
2020-02-10 11:13:49 +0800

    Update docker_start.sh

--- docker_start.sh
+++ docker_start.sh
@@ -13,16 +13,18 @@ echo ${_LOCUST_OPTS} >&2
 
 if [ "${LOCUST_MODE}" = "master" ]; then
     _LOCUST_OPTS="${_LOCUST_OPTS} --master"
+    echo "Mode note 1/3 : Starting Locust in ${LOCUST_MODE} mode..."
+    
 elif [ "${LOCUST_MODE}" = "slave" ]; then
     if [ -z "${LOCUST_MASTER_HOST}" ]; then
         echo "ERROR: MASTER_HOST is empty. Slave mode requires a master" >&2
         exit 1
     fi
-
+    echo "Mode note 2/3 : Starting Locust in ${LOCUST_MODE} mode..."
     _LOCUST_OPTS="${_LOCUST_OPTS} --slave --master-host=${LOCUST_MASTER_HOST} --master-port=${LOCUST_MASTER_PORT:-5557}"
 fi
 
-echo "Starting Locust in ${LOCUST_MODE} mode..."
+echo "Mode note 3/3 : Starting Locust in ${LOCUST_MODE} mode..."
 echo "$ locust ${LOCUST_OPTS} ${_LOCUST_OPTS}"
 
 exec locust ${LOCUST_OPTS} ${_LOCUST_OPTS}
