8e0cf6ca5e877f9b902c2d2f980701fe7307f000
ulshi <46480421+ulshi@users.noreply.github.com>
2020-02-12 16:48:16 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,20 +3,20 @@ class WebsiteTasks(TaskSet):
 
     @task(10)
     def index(self):
-        self.client.get("http://10.0.1.58:30051/2020/02/12/git3/")
+        self.client.get("http://10.0.1.167:30051/2020/02/12/git3/")
 
     @task(10)
     def index(self):
-        self.client.get("http://10.0.1.58:30051/2020/02/12/feiyan3/")
+        self.client.get("http://10.0.1.167:30051/2020/02/12/feiyan3/")
 
     @task(10)
     def index(self):
-        self.client.get("http://10.0.1.58:30051/2020/02/12/feiyan1/")
+        self.client.get("http://10.0.1.167:30051/2020/02/12/feiyan1/")
 
 
     @task(10)
     def about(self):
-        response=self.client.get("http://10.0.1.58:30051/2020/02/12/bbc2/")
+        response=self.client.get("http://10.0.1.167:30051/2020/02/12/bbc2/")
         '''print("Response status code:", response.status_code)
         print("Response content:", response.text)'''
 
