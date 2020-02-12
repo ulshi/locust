f8d6e912571698b2dbdd449078340289a8b19686
ulshi <46480421+ulshi@users.noreply.github.com>
2020-02-12 16:37:47 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,22 @@
 from locust import HttpLocust, TaskSet, task, between
 class WebsiteTasks(TaskSet):
-    @task(1)
-    def on_start(self):
-        self.client.post("http://159.138.93.135:8080/login", {
-            "username": "admin_wp",
-            "password": "EL&FK6xsZ&oZCL^MIq"
-        })
 
     @task(10)
     def index(self):
-        self.client.get("http://159.138.93.135:8080/2020/01/15/docker-run/")
+        self.client.get("http://10.0.1.58:30051/2020/02/12/git3/")
+
+    @task(10)
+    def index(self):
+        self.client.get("http://10.0.1.58:30051/2020/02/12/feiyan3/")
+
+    @task(10)
+    def index(self):
+        self.client.get("http://10.0.1.58:30051/2020/02/12/feiyan1/")
 
 
     @task(10)
     def about(self):
-        response=self.client.get("http://159.138.93.135:8080/2020/01/15/docker-import/")
+        response=self.client.get("http://10.0.1.58:30051/2020/02/12/bbc2/")
         '''print("Response status code:", response.status_code)
         print("Response content:", response.text)'''
 
