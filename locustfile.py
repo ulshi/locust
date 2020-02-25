e5f59ae95ecfe0670833af571e51c29a651510b8
ulshi <46480421+ulshi@users.noreply.github.com>
2020-02-25 20:57:06 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -2,7 +2,7 @@ from locust import HttpLocust, TaskSet, task, between
 class WebsiteTasks(TaskSet):
     def index(self):
         payload={'uniqueCode': 'oR0PfwNo3DTzkm4zb7BHawFB', 'cityCode': '3009', 'isJsonp': '1', 'terminalID': '2', 'subCityCode': None}
-        self.client.get("http://114.116.172.56:8080/eUrbanMIS/api/pnemonic/ifbind",params=payload)
+        self.client.get("http://114.116.172.56:8080/eUrbanMIS/api/pnemonic/ifbind")
         
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
