3adf98cb879297fd5787aa29a9b777539188d466
ulshi <46480421+ulshi@users.noreply.github.com>
2020-02-26 09:49:11 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
 from locust import HttpLocust, TaskSet, task, between
 class WebsiteTasks(TaskSet):
     def index(self):        
-        self.client.get("http://114.116.172.56:8080/eGovaPublic/view/mobileapp/publicinspect/index.html?cityCode=3009&isWechat=1&other=1&moduleId=266&openId=oR0PfwNo3DTzkm4zb7BHawFB#/epidemicHome?VNK=c18082cb")
+        self.client.get("http://159.138.106.243:30051/2020/02/13/mssql-backup-summary/")
         
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
