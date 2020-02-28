5dea66a028b5cb97d7b6796cd19372544a65390b
ulshi <46480421+ulshi@users.noreply.github.com>
2020-02-28 15:09:29 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,15 @@
 from locust import HttpLocust, TaskSet, task, between
 class WebsiteTasks(TaskSet):
-    def index(self):        
-        self.client.get("http://159.138.106.243:30051/2020/02/13/mssql-backup-summary/")
-        
+
+    @task(10)
+    def index(self):
+        self.client.get("http://10.0.1.167:30051/2020/02/12/feiyan1/")
+
+
+    @task(10)
+    def about(self):
+        response=self.client.get("http://10.0.1.167:30051/2020/02/12/bbc2/")
+
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
-    wait_time = between(2, 4)
+    wait_time = between(5, 15)
