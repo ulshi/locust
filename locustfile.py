28d559b74c86d9c290abfe6d4890f459b47f0e96
ulshi <46480421+ulshi@users.noreply.github.com>
2020-03-02 20:40:26 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -7,8 +7,13 @@ class WebsiteTasks(TaskSet):
 
 
     @task(10)
-    def about(self):
-        response=self.client.get("http://10.0.1.167:30051/2020/02/12/bbc2/")
+    def login(self):
+        self.client.post("http://114.116.172.56:8080/eUrbanMIS/api/pnemonic/ifbind",data=None,json={"uniqueCode": "oR0PfwNo3DTzkm4zb7BHawFB",
+                                                                                                    "cityCode": "3001",
+                                                                                                    "isJsonp": "1",
+                                                                                                    "terminalID": "2",
+                                                                                                    "subCityCode": ""
+                                                                                                   })
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
