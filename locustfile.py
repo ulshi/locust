a6dbb2409f9ed45f5536bfa1d9abcbf69db31977
ulshi <46480421+ulshi@users.noreply.github.com>
2020-03-03 10:06:33 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,19 +1,31 @@
 from locust import HttpLocust, TaskSet, task, between
+import json
+
+host = "http://114.116.172.56:8080"
+loginUrl = "/eUrbanMIS/api/pnemonic/ifbind"
+
 class WebsiteTasks(TaskSet):
 
     @task(10)
     def index(self):
         self.client.get("http://10.0.1.167:30051/2020/02/12/feiyan1/")
-
+        
 
     @task(10)
-    def login(self):
-        self.client.post("http://114.116.172.56:8080/eUrbanMIS/api/pnemonic/ifbind",data=None,json={"uniqueCode": "oR0PfwNo3DTzkm4zb7BHawFB",
-                                                                                                    "cityCode": "3001",
-                                                                                                    "isJsonp": "1",
-                                                                                                    "terminalID": "2",
-                                                                                                    "subCityCode": ""
-                                                                                                   })
+    def on_start(self):
+        data = json.dumps({
+            "uniqueCode": "oR0PfwNo3DTzkm4zb7BHawFB",                                               
+            "cityCode": "3001",                                                      
+            "isJsonp": "1",                                                          
+            "terminalID": "2",                                                                                                    
+            "subCityCode": ""                                                                                                   
+        })
+        
+        response = self.client.request(method="POST", url= loginUrl,
+                                       data = data,
+                                       headers = None)
+        print("LOGIN RESULT:", response.status_code, response.content)
+ 
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
