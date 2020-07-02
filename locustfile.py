deb072621b99d1af809ac66f2644312adc5b7d09
ulshi <46480421+ulshi@users.noreply.github.com>
2020-07-02 20:52:18 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,8 @@
 from locust import HttpLocust, TaskSet, task, between
 import json
 
-host = "http://119.3.135.230:80"
-loginUrl = "/eUrbanMIS/login/validpassword"
 
 class WebsiteTasks(TaskSet):
-
-    @task(10)
-    def index(self):
-        self.client.post("http://ln3jd7py4cb0tr4r.zhutingxue.ap-southeast-3.huaweicse.com/ui/rest/userservice/registe",{
-            "telNum": "5768578",
-            "user": "675478"   
-        })      
 
     @task(10)
     def index(self):
@@ -20,4 +11,4 @@ class WebsiteTasks(TaskSet):
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
     host = host
-    wait_time = between(0,0)
+    wait_time = between(0,10)
